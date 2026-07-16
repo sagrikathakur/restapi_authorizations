81906aacd277b01d874b97199601523e2ab59098
sagrika thakur <sagrikathakur68@gmail.com>
2026-07-16 17:31:38 +0530

    fixing an error

---
+++ react-projects/vite-food/src/Context/StoreContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      const updated = { ...prev };
+      if (updated[itemId] > 1) {
+        updated[itemId] -= 1;
+      } else {
+        delete updated[itemId];
+      }
+      return updated;
+    });
+  }
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        if (itemInfo) {
+          totalAmount += itemInfo.price * cartItems[item];
+        }
+      }
+    }
+    return totalAmount;
+  }
+
+  const ContextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  }
+
+  return (
+    <StoreContext.Provider value={ContextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  )
+}
+
+export default StoreContextProvider
